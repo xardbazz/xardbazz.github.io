b2a0ee02a65fc38efca00c064f60bff1d8ced92d
Kamal Issa <kamalissa96@gmail.com>
2025-04-15 21:39:54 +0300

    Fix image paths for GitHub Pages

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,22 +2,22 @@
     export const ProjectList =[
         {
             name: "Farming Simulator",
-            image: "/images/Capture1.png",
+            image: "/images/Capture1.PNG",
             skills: " C#, Unity, Adobe",
         },
         {
             name: "Issa-Tech",
-            image: "/images/Capture2.png",
+            image: "/images/Capture2.PNG",
             skills: " PHP, HTML, CSS, MySql, Xampp",
         },
         {
             name: "Personal-Portfolio",
-            image: "/images/Capture3.png",
+            image: "/images/Capture3.PNG",
             skills: "JavaScript, React, HTML, CSS, Node.js",
         },
         {
             name: "Medicial application",
-            image: "/images/Capture4.png",
+            image: "/images/Capture4.PNG",
             skills: "Adobe XD",
         },
 
